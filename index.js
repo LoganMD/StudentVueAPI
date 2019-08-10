cbfa2ced55db66a49525f6eeb80ea8f1041b6987
Logandev_ <ldoose@live.com>
2019-08-09 17:58:18 -0700

    Create index.js
    again, needs cleaning, but still works
    server is hosted on port 3000

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const StudentVue = require('./vue.js');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    res.json({"message": "StudentVUE API by Logandev_ and kachang"});
+});
+
+app.post('/getStudentInfo', (req, res) => {
+  console.log("index.js: getStudentInfo requested, summoning vue.js")
+    if(!req.body.id) {
+        return res.status(400).send({
+            message: "id content can not be empty"
+        });
+    }
+    if(!req.body.password) {
+        return res.status(400).send({
+            message: "id content can not be empty"
+        });
+    }
+    if(!req.body.url) {
+        return res.status(400).send({
+            message: "id content can not be empty"
+        });
+    }
+    StudentVue.getStudentInfo(req.body.id, req.body.password, req.body.url).then(resp => res.send(resp))
+})
+
+app.post('/getSchoolInfo', (req, res) => {
+    if(!req.body.id) {
+        return res.status(400).send({
+            message: "id content can not be empty"
+        });
+    }
+    if(!req.body.password) {
+        return res.status(400).send({
+            message: "id content can not be empty"
+        });
+    }
+    if(!req.body.url) {
+        return res.status(400).send({
+            message: "id content can not be empty"
+        });
+    }
+
+    StudentVue.getSchoolInfo(req.body.id, req.body.password, req.body.url).then(resp => res.send(resp))
+})
+
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
