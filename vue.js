7b5459066d126b38b1764e70d286e3d709ed77f2
Logandev_ <ldoose@live.com>
2019-08-09 17:57:06 -0700

    Create vue.js
    needs some cleaning, but it works atm

---
+++ vue.js
@@ -0,0 +1,30 @@
+function getSTInfo(id, password, url) {
+    var spawn = require("child_process").spawn;
+    var process = spawn('python',["./StudentVue/getStudentInfo.py", id, password, url] );
+    return new Promise(resolve => {
+      process.stdout.on('data', function(data) {
+        resolve(data.toString());
+      })
+  });
+}
+
+async function getSInfo(id, password, url) {
+  var result = await getSTInfo(id, password, url)
+  return result
+}
+
+function getSCInfo(id, password, url) {
+    return new Promise(resolve => {
+      process.stdout.on('data', function(data) {
+        resolve(data.toString());
+      })
+  });
+}
+
+async function getSCHInfo(id, password, url) {
+  var result = await getSCInfo(id, password, url)
+  return result
+}
+
+module.exports.getSchoolInfo = getSCHInfo
+exports.getStudentInfo = getSInfo
